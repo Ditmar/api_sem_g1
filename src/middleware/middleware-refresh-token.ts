fd93e7825048a44e3538d6d4626c7e412ec3f067
BetzabeMarino <marinocondoribetzabe@gmail.com>
2023-12-10 23:48:49 -0400

    feature(common): fixing bugs and refactoring code, deleting comments [SEM - 12]

--- src/middleware/middleware-refresh-token.ts
+++ src/middleware/middleware-refresh-token.ts
@@ -1,29 +1,3 @@
-// import { Request, Response, NextFunction } from 'express';
-// import HttpStateCodes from '../utils/http-state-codes';
-
-// export const RefreshTokenMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  
-//   const expectedFields = ['refreshToken'];
-//   const validKeys = Object.keys(req.body);
-
-//   const missingFields = expectedFields.filter((field) => !validKeys.includes(field));
-
-//   const extraFields = validKeys.filter((key) => !expectedFields.includes(key));
-
-//   if (missingFields.length > 0) {
-//     return res.json({
-//       message:`Faltan los siguientes campos en la solicitud: ${missingFields.join(', ')}`, 
-//       status:HttpStateCodes.BAD_REQUEST});
-//   }
-
-//   if (extraFields.length > 0) {
-//     return res.json({message:`Campos no válidos en la solicitud: ${extraFields.join(', ')}`, status:HttpStateCodes.BAD_REQUEST});
-//  }
-
-//   next();
-// };
-
-
 import { Request, Response, NextFunction } from 'express';
 import HttpStateCodes from '../utils/http-state-codes';
 import { resfreshTokenSchema } from '../schemas/refresh-token-schemas';
